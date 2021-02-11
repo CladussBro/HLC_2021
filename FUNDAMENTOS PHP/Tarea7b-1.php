4d68e361414483a7a68f450ee080391629ace230
ClaudiaBorrachero <alum.cnereaborracherom@iesalixar.org>
2021-02-11 16:18:52 +0100

    HLC

--- FUNDAMENTOS PHP/Tarea7b-1.php
+++ FUNDAMENTOS PHP/Tarea7b-1.php
@@ -4,24 +4,32 @@
     $A="12"; //en binario 110001110010;
     $B="23"; //en binario 110010110011;
     //operador &;
+    //Los bits activos en ambos son activados
     print_r($a&$b);
     echo "<br>";
     print_r($A&$B);
     echo "<br>";
     //operador |;
+    //Los bits activos ya sea en a o b son activados
     print_r($a|$b);
     echo "<br>";
     print_r($A|$B);
     echo "<br>";
     //operador ^;
+    //Los bits activos de a o b son activados, pero sólo de uno de ellos
     print_r($a^$b);
     print_r($A^$B);
     echo "<br>";
     //repite el código para el operador <<;
+    //Desplaza los bits de a tantos pasos a la izquierda como tenga b, multiplicar por dos
+    print_r('Operadores "<<" ');
+    print_r($a<<$b);
     echo "<br>";
     //repite el código para el operador >>;
+    //Desplaza los bits de a tantos pasos a la derecha como tenga b, dividir por dos
+    print_r('Operadores ">>" ');
+    print_r($a>>$b);
     echo "<br>";
     //operador ~
     print_r(~$a);
-
 ?>
